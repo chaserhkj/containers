4c9cdda0ae353f07a9afd18a4b21ac93e77dc6fc
Chaser Huang <huangkangjing@gmail.com>
2026-01-17 01:49:16 -0500

    Improvements to comfyui entrypoint script

--- service/comfyui-runtime/entrypoint.sh
+++ service/comfyui-runtime/entrypoint.sh
@@ -6,27 +6,25 @@ CLI_ARGS+=" --listen 127.8.8.1 --port 8000"
 CLI_ARGS+=" --log-stdout"
 CLI_ARGS+=" --use-pytorch-cross-attention"
 
-if [[ $TORCH_BACKEND == rocm* ]]; then
-    # ROCM-specific optimizations
-    # These are architecture and device dependent, may need further tuning
-    # or some device-id-based guards here
-    export MIGRAPHX_MLIR_USE_SPECIFIC_OPS="attention"
-    export TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
-    export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
+# ROCM-specific optimizations
+# These are architecture and device dependent
+# Comment out unneeded settings
+export MIGRAPHX_MLIR_USE_SPECIFIC_OPS="attention"
+export TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
+export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
 
-    export FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE"
-    export FLASH_ATTENTION_TRITON_AMD_AUTOTUNE="TRUE"
+export FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE"
+export FLASH_ATTENTION_TRITON_AMD_AUTOTUNE="TRUE"
 
-    # Force use HIPBLASLT, since rocblas is giving a segfault
-    export MIOPEN_GEMM_ENFORCE_BACKEND=5
-    # Same thing for MIGraphX, this is related to ONNX
-    export MIGRAPHX_SET_GEMM_PROVIDER=hipblaslt
-    export MIOPEN_FIND_MODE=5
+# Force use HIPBLASLT, since rocblas is giving a segfault
+export MIOPEN_GEMM_ENFORCE_BACKEND=5
+# Same thing for MIGraphX, this is related to ONNX
+export MIGRAPHX_SET_GEMM_PROVIDER=hipblaslt
+export MIOPEN_FIND_MODE=5
 
-    #export MIOPEN_LOG_LEVEL=5
-    #export MIOPEN_ENABLE_LOGGING=1
-    #export MIOPEN_ENABLE_LOGGING_CMD=1
-fi
+#export MIOPEN_LOG_LEVEL=5
+#export MIOPEN_ENABLE_LOGGING=1
+#export MIOPEN_ENABLE_LOGGING_CMD=1
 
 source /venv/bin/activate
 echo "[INFO] Prestart ENV exported"
@@ -37,7 +35,9 @@ if [[ -f /app/install.sh ]]; then
     bash /app/install.sh
 fi
 
-if [[ ! -f /venv/COMFYUI_CUSTOM_NODES_FIXED ]]; then
+if [[ ! -f /venv/COMFYUI_CUSTOM_NODES_FIXED ]] || 
+    [[ ! -f /app/COMFYUI_CUSTOM_NODES_UPDATED ]] || 
+    [[ /app/COMFYUI_CUSTOM_NODES_UPDATED -nt /venv/COMFYUI_CUSTOM_NODES_FIXED ]]; then
     echo "[INFO] Running ComfyUI-Manager fix"
     python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py fix all
     touch /venv/COMFYUI_CUSTOM_NODES_FIXED

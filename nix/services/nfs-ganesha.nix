09d6b9cdd9d77bef69140dcf78ab66ec4714ebfd
Chaser Huang <huangkangjing@gmail.com>
2026-04-08 03:47:22 -0400

    More configurations for ganesha build

--- nix/services/nfs-ganesha.nix
+++ nix/services/nfs-ganesha.nix
@@ -4,18 +4,58 @@
       name = "ghcr.io/chaserhkj/containers/nfs-ganesha";
       tag = "latest";
       config = with pkgs; let
-        ganeshaWithoutRPCBind = nfs-ganesha.overrideAttrs (
+        ganeshaInputConfigured = nfs-ganesha.override {
+          useCeph = false;
+          useDbus = false;
+        };
+        ganeshaConfigured = ganeshaInputConfigured.overrideAttrs (
           oldAttrs: {
+            outputs = ["out"];
             cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+              # Disable other protocols and only leave NFS4/RDMA
+              "-DUSE_9P=OFF"
+              "-DUSE_NFS3=OFF"
+              "-DUSE_RQUOTA=OFF"
+              "-DUSE_NFS4=ON"
+              "-DUSE_NFS_RDMA=ON"
+              # Enabled features
+              "-DUSE_FSAL_VFS=ON"
+              "-DUSE_FSAL_PROXY_V4=ON"
+              "-DENABLE_VFS_POSIX_ACL=ON"
+              "-DUSE_ACL_MAPPING=ON"
+              "-DUSE_MONITORING=ON"
+              "-DUSE_CAPS=ON"
+              # Disable rpcbind and portmapper for NFS4 only
               "-D_NO_TCP_REGISTER=ON"
               "-DRPCBIND=OFF"
+              # Unused features
+              "-DUSE_DBUS=OFF"
+              "-DUSE_ADMIN_TOOLS=OFF"
+              "-DUSE_GUI_ADMIN_TOOLS=OFF"
+              "-DUSE_RADOS_RECOV=OFF"
+              "-DUSE_GSS=OFF"
+              "-DUSE_MAN_PAGE=OFF"
+              "-DUSE_NFSIDMAP=OFF"
+              # Unused FSAL drivers
+              "-DUSE_FSAL_PROXY_V3=OFF"
+              "-DUSE_FSAL_LUSTRE=OFF"
+              "-DUSE_FSAL_LIZARDFS=OFF"
+              "-DUSE_FSAL_KVSFS=OFF"
+              "-DUSE_FSAL_CEPH=OFF"
+              "-DUSE_FSAL_RGW=OFF"
+              "-DUSE_FSAL_SAUNAFS=OFF"
+              "-DUSE_FSAL_XFS=OFF"
+              "-DUSE_FSAL_GPFS=OFF"
+              "-DUSE_FSAL_GLUSTER=OFF"
+              "-DUSE_FSAL_NULL=OFF"
+              "-DUSE_FSAL_MEM=OFF"
             ];
           }
         );
       in {
         Entrypoint = [ 
           "${tini}/bin/tini" "--"
-          "${ganeshaWithoutRPCBind}/bin/ganesha.nfsd"
+          "${ganeshaConfigured}/bin/ganesha.nfsd"
         ];
         Cmd = [
           "-F" "-x" "-L" "/dev/stdout" "-f"

411b46b9c46cd172e6dfbcec8a23978600618726
Chaser Huang <huangkangjing@gmail.com>
2026-04-11 19:14:58 -0400

    Add flakeModules optional import

--- nix/lib/modules.nix
+++ nix/lib/modules.nix
@@ -1,10 +1,11 @@
-{flake-parts-lib, withSystem, ...}: let 
+{flake-parts-lib, withSystem, inputs, ...}: let 
   inherit(flake-parts-lib) importApply;
   importApplyLocalDeps = module: importApply module { inherit withSystem; };
   mkImageWithEntryPoint = importApplyLocalDeps ./mkImageWithEntrypoint.nix;
   containerizedApp = importApplyLocalDeps ./containerizedApp.nix;
 in {
   imports = [
+    inputs.flake-parts.flakeModules.flakeModules
     mkImageWithEntryPoint
   ];
   flake.flakeModules = {

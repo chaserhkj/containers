b0611a6bc82c24a928379237b04da50803694dd0
Chaser Huang <huangkangjing@gmail.com>
2026-04-30 10:35:52 -0400

    Buildable amd-ctk

--- nix/packages/amd-ctk.nix
+++ nix/packages/amd-ctk.nix
@@ -1,20 +1,24 @@
 {
   lib,
-  fetchFromGithub,
+  fetchFromGitHub,
   buildGoModule
 }: buildGoModule (finalAttrs: {
   pname = "amd-ctk";
   version = "1.3.0";
-  src = fetchFromGithub {
+  src = fetchFromGitHub {
     owner = "ROCm";
     repo = "container-toolkit";
     tag = "v${finalAttrs.version}";
-    hash = lib.fakeHash;
+    hash = "sha256-+YqVm/u0PYvyoHUSwRapByvmIunZUfcPV5rYDFW7Ifs=";
   };
 
-  vendorHash = null;
+  vendorHash = "sha256-w7QJBSRRsvxUaXNiXw3OnkZCcKJgwp/WTJQcDJ1msaA=";
 
   subPackages = [
     "cmd/amd-ctk"
   ];
+  
+  preCheck = ''
+    export AMD_CTK_PATH=$GOPATH/bin/amd-ctk
+  '';
 })

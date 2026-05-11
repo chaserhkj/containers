5e00dc4566b25ef1b9e129b2c6c355987e3fcae5
Chaser Huang <huangkangjing@gmail.com>
2026-05-11 16:05:43 -0400

    Reorg nix2container build and add term-agents image

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix2container.url = "github:nlewo/nix2container";
     devshell.url = "github:numtide/devshell";
+    llm-agents.url = "github:numtide/llm-agents.nix";
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -14,4 +15,8 @@
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     };
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
+  };
 }
--- nix/environments/term-agents.nix
+++ nix/environments/term-agents.nix
@@ -2,7 +2,8 @@
   imports = [
     inputs.devshell.flakeModule
   ];
-  perSystem = {pkgs, self', ...}: with pkgs; let 
+  perSystem = {inputs', pkgs, self', ...}: with pkgs; let 
+      inherit (inputs'.nix2container.packages) nix2container;
       nixPkg = pkgs.nixVersions.latest;
       nixConfig = pkgs.writeTextDir "etc/nix/nix.conf"
         ''
@@ -11,18 +12,33 @@
           build-users-group =
           sandbox = false
         '';
+      corePkgs = with pkgs; [
+        coreutils-full
+        util-linux
+        busybox
+        curl
+        nixPkg
+        git
+      ];
+      agentPkgs = with inputs'.llm-agents.packages; [
+        opencode
+
+        openskills
+        tuicr
+        ck
+      ];
     in {
       devshells.term-agents = {
-        packages = [
-          pkgs.hello
+        packages = corePkgs ++ agentPkgs ++ [
         ];
       };
       imagesWithFixedEntrypointVariant.term-agents = {
-        name = "term-agents";
+        name = "ghcr.io/chaserhkj/containers/term-agents";
+        tag = "latest";
         config = {
-          entrypoint = ["${nixPkg}/bin/nix"];
-          cmd = [];
+          entrypoint = ["${self'.devShells.term-agents.flakeApp.program}"];
         };
+        initializeNixDatabase = true;
         copyToRoot = [
           nixConfig
         ];

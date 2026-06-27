e2c69cf5fabff392ec36dd94a9e995cf6e17ed87
Chaser Huang <huangkangjing@gmail.com>
2026-06-27 05:45:28 -0400

    Remove cecli and some cleanups

--- nix/packages.nix
+++ nix/packages.nix
@@ -3,7 +3,6 @@
     packages = {
       open-code-review = pkgs.callPackage ./packages/open-code-review.nix {};
       mozilla-cq = pkgs.callPackage ./packages/mozilla-cq.nix {};
-      cecli = pkgs.callPackage ./packages/cecli.nix {};
     };
   };
 }

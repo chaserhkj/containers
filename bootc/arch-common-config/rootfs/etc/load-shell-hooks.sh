50931fd64d6e0520b4bc30f5192a3117937c1699
Chaser Huang <huangkangjing@gmail.com>
2026-07-16 05:30:01 -0400

    fix(hooks): save and restore nullglob to handle '*' on empty directory

--- bootc/arch-common-config/rootfs/etc/load-shell-hooks.sh
+++ bootc/arch-common-config/rootfs/etc/load-shell-hooks.sh
@@ -1,5 +1,9 @@
+shopt -q nullglob && _nullglob_set=1 || _nullglob_set=0
+shopt -s nullglob
 if [[ -d /etc/shell-hooks ]]; then
     for hook_script in /etc/shell-hooks/*; do
         [[ -f "$hook_script" ]] && source "$hook_script"
     done
 fi
+(( ! $_nullglob_set )) && shopt -u nullglob
+unset _nullglob_set

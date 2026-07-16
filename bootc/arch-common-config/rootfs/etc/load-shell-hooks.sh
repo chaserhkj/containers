89fae69c60472edb974ca84ef303071f803fe924
Chaser Huang <huangkangjing@gmail.com>
2026-07-16 08:50:43 -0400

    refactor(load-shell-hooks.sh): simplify empty directory check by using `ls`

--- bootc/arch-common-config/rootfs/etc/load-shell-hooks.sh
+++ bootc/arch-common-config/rootfs/etc/load-shell-hooks.sh
@@ -1,9 +1,6 @@
-shopt -q nullglob && _nullglob_set=1 || _nullglob_set=0
-shopt -s nullglob
-if [[ -d /etc/shell-hooks ]]; then
-    for hook_script in /etc/shell-hooks/*; do
+hook_dir=/etc/shell-hooks
+if [[ -d "$hook_dir" ]] && [[ -n "$(ls "$hook_dir")" ]]; then
+    for hook_script in "$hook_dir"/*; do
         [[ -f "$hook_script" ]] && source "$hook_script"
     done
 fi
-(( ! $_nullglob_set )) && shopt -u nullglob
-unset _nullglob_set
